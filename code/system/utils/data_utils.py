b2ed63e0f20d75d60f3bb8b1ca034c76bfbae078
qixiang <qixiang1009@gmail.com>
2024-06-28 02:31:26 +0800

    bug fix

--- code/system/utils/data_utils.py
+++ code/system/utils/data_utils.py
@@ -5,8 +5,9 @@ import torch
 
 def read_data(dataset, idx, is_train=True):
     if is_train:
-        train_data_dir = os.path.join('../dataset', dataset, 'train/')
-
+        train_data_dir = os.path.join('../../dataset', dataset, 'train/')
+        #train_data_dir = "..\\dataset\\" + dataset + "\\train\\"
+        
         train_file = train_data_dir + str(idx) + '.npz'
         with open(train_file, 'rb') as f:
             train_data = np.load(f, allow_pickle=True)['data'].tolist()
@@ -14,7 +15,8 @@ def read_data(dataset, idx, is_train=True):
         return train_data
 
     else:
-        test_data_dir = os.path.join('../dataset', dataset, 'test/')
+        test_data_dir = os.path.join('../../dataset', dataset, 'test/')
+        #test_data_dir = "..\\dataset\\" + dataset + "\\test\\"
 
         test_file = test_data_dir + str(idx) + '.npz'
         with open(test_file, 'rb') as f:
